69079d9ec9cf59a9b564a76792adb403a05159db
Adilius <leugue-gamer@hotmail.com>
2021-03-16 22:46:45 +0100

    Fixed stuff, added contact

--- js/custom.js
+++ js/custom.js
@@ -4,21 +4,24 @@
 window.onload = function(){
 
     //API call to display page last updated
-    fetch('https://api.github.com/repos/adilius/adilius.github.io')
-    .then(response => response.json())
-    .then(data => {
-        var updateDate = new Date(data.updated_at)
-        var currentDate = new Date();
-        var dayDifference = Math.floor(Math.abs(currentDate - updateDate)/(1000 * 60 * 60 * 24))
-
-        if(dayDifference == 0){
-            document.getElementById("repoDate").innerHTML = " today";
-        }else if(dayDifference == 1){
-            document.getElementById("repoDate").innerHTML += dayDifference +" day ago";
-        }else{
-            document.getElementById("repoDate").innerHTML += dayDifference +" days ago";
-        }
-    })
+    async function asyncFetch() {
+        fetch('https://api.github.com/repos/adilius/adilius.github.io')
+        .then(response => response.json())
+        .then(data => {
+            var updateDate = new Date(data.updated_at)
+            var currentDate = new Date();
+            var dayDifference = Math.floor(Math.abs(currentDate - updateDate)/(1000 * 60 * 60 * 24))
+    
+            if(dayDifference == 0){
+                document.getElementById("repoDate").innerHTML = " today";
+            }else if(dayDifference == 1){
+                document.getElementById("repoDate").innerHTML += dayDifference +" day ago";
+            }else{
+                document.getElementById("repoDate").innerHTML += dayDifference +" days ago";
+            }
+        })
+    
+    }
 
 
 
@@ -53,7 +56,7 @@ window.onload = function(){
 
 
 
-	
+	asyncFetch()
 };
 
 // Grabbed this from Stack Overflow.
