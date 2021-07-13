f35e10626a3f3ebce6a2a44bef8f6566122545b2
Adilius <leugue-gamer@hotmail.com>
2021-07-13 20:42:18 +0200

    Dynamic pages now

--- js/custom.js
+++ js/custom.js
@@ -1,72 +1,121 @@
 "use strict";
 
-//Enable bootstrap tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
 //When windows has finished loading
-window.onload = function(){
+window.onload = function () {
 
-    //API call to display page last updated
-    async function asyncFetch() {
-        fetch('https://api.github.com/repos/adilius/adilius.github.io')
-        .then(response => response.json())
-        .then(data => {
-            var updateDate = new Date(data.updated_at)
-            var currentDate = new Date();
-            var dayDifference = Math.floor(Math.abs(currentDate - updateDate)/(1000 * 60 * 60 * 24))
-    
-            if(dayDifference == 0){
-                document.getElementById("repoDate").innerHTML = " today";
-            }else if(dayDifference == 1){
-                document.getElementById("repoDate").innerHTML += dayDifference +" day ago";
-            }else{
-                document.getElementById("repoDate").innerHTML += dayDifference +" days ago";
-            }
-        })
-    
-    }
+    //Loads nav and footer from files
+    loadHtml("nav", "html/nav.html")
+    loadHtml("footer", "html/footer.html")
 
-	var messageCopy = "\"Click to copy email address\"";
-	var messageSuccess = "\"Email address copied to clipboard!\"";
+    //Enables Bootstrap Tooltips
+    enableBootstrapTooltips();
 
-    var styleElem = document.head.appendChild(document.createElement("style"));
-    var message = ".mailto-message:after {content: " + messageCopy + ";}"
-    styleElem.innerHTML = message;
+    //Setup email copy function
+    emailCopy();
 
-	// On click, get href and remove 'mailto:' from value
-	// Store email address in a variable.
-    var mailto = document.getElementById("email")
-    mailto.addEventListener("click", () => {
-        copyToClipboard();
-        var message = ".mailto-message:after {content: " + messageSuccess + ";}"
-        styleElem.innerHTML = message;
-		setTimeout(function () {
-            var message = ".mailto-message:after {content: " + messageCopy + ";}"
-            styleElem.innerHTML = message;
-		}, 2000);
-
-    })
-
-
-
-    //Get repo update date
-	asyncFetch();
+    //Get repository update date
+    asyncFetch();
 
     //Run gifffer script
     Gifffer();
 
 };
 
-// Grabbed this from Stack Overflow.
-// Copies the email variable to clipboard
-function copyToClipboard() {
-	var dummy = document.createElement("input");
-	document.body.appendChild(dummy);
-	dummy.setAttribute("value", "adilaboulkacim@gmail.com");
-	dummy.select();
-	document.execCommand("copy");
-	document.body.removeChild(dummy);
+
+
+//API call to display page last updated
+async function asyncFetch() {
+    fetch('https://api.github.com/repos/adilius/adilius.github.io')
+        .then(response => response.json())
+        .then(data => {
+            var updateDate = new Date(data.updated_at)
+            var currentDate = new Date();
+            var dayDifference = Math.floor(Math.abs(currentDate - updateDate) / (1000 * 60 * 60 * 24))
+
+            if (dayDifference == 0) {
+                document.getElementById("repoDate").innerHTML = " today";
+            } else if (dayDifference == 1) {
+                document.getElementById("repoDate").innerHTML += dayDifference + " day ago";
+            } else {
+                document.getElementById("repoDate").innerHTML += dayDifference + " days ago";
+            }
+        })
+
 }
+
+//Enables email to be copied by clicking the email icon
+function emailCopy() {
+    var messageCopy = "\"Click to copy email address\"";
+    var messageSuccess = "\"Email address copied to clipboard!\"";
+
+    var styleElem = document.head.appendChild(document.createElement("style"));
+    var message = ".mailto-message:after {content: " + messageCopy + ";}"
+    styleElem.innerHTML = message;
+
+    // On click, get href and remove 'mailto:' from value
+    // Store email address in a variable.
+    var mailto = document.getElementById("email")
+    mailto.addEventListener("click", () => {
+        copyToClipboard();
+        var message = ".mailto-message:after {content: " + messageSuccess + ";}"
+        styleElem.innerHTML = message;
+        setTimeout(function () {
+            var message = ".mailto-message:after {content: " + messageCopy + ";}"
+            styleElem.innerHTML = message;
+        }, 2000);
+
+    })
+}
+
+//emailCopy helper function
+function copyToClipboard() {
+    var dummy = document.createElement("input");
+    document.body.appendChild(dummy);
+    dummy.setAttribute("value", "adilaboulkacim@gmail.com");
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+}
+
+//Enable bootstrap tooltips
+function enableBootstrapTooltips() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+}
+
+/* 
+ * @param {string} parentElementId - The ID of the DOM element to load into
+ * @param {string} htmlFilePath - The path of the HTML file to load
+ */
+const loadHtml = function (parentElementId, filePath) {
+    const init = {
+        method: "GET",
+        headers: { "Content-Type": "text/html" },
+        mode: "cors",
+        cache: "default"
+    };
+
+    const req = new Request(filePath, init);
+    fetch(req)
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (body) {
+            var oldChild = document.getElementById(parentElementId);
+            console.log('Old Child');
+            console.log(oldChild);
+
+            let doc = new DOMParser().parseFromString(body, 'text/html')
+            let newChild = doc.body.firstChild;
+            console.log('New Child');
+            console.log(newChild);
+
+            var parent = document.getElementById(parentElementId).parentNode;
+            console.log('Parent');
+            console.log(parent);
+
+            oldChild.replaceWith(newChild)
+        });
+};
