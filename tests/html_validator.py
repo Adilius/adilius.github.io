da54fdd1ef358c5e5c57fa5d6d3b43403e917a83
Adilius <leugue-gamer@hotmail.com>
2021-07-14 23:24:48 +0200

    Added test

---
+++ tests/html_validator.py
@@ -0,0 +1,22 @@
+import requests
+
+URL = "http://validator.w3.org/nu/"
+
+PARAMS = {  'doc': 'https://bradgarropy.com/',
+            'out': 'json'}
+
+r = requests.get(url = URL, params = PARAMS)
+
+data = r.json()
+
+data.pop('url', None)
+
+error_count = 0
+
+for message in data['messages']:
+    if 'type' in message:
+        if message.get('type') == 'error':
+            error_count += 1
+            print(error_count,message.get('type'))
+
+print('Error count:', error_count)
