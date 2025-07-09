e34d1042e550a018055f3e11f608ab3d5c389b5f
Adil <43440295+Adilius@users.noreply.github.com>
2025-07-09 19:15:51 +0200

    Merge dev (#13)
    * Removed old code
    
    * New project
    
    ---------
    
    Co-authored-by: AdilAboulkacim <adil.aboulkacim@consid.se>

---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+export default function Blog({ posts }) {
+  return (
+    <>
+      <h1>Welcome to My Blog</h1>
+      <p>Here are my latest posts:</p>
+      {posts.map(({ slug, frontmatter }) => (
+        <div key={slug}>
+          <h2>
+            <Link href={`/blog/${slug}`}>{frontmatter.title}</Link>
+          </h2>
+          <p>{frontmatter.date}</p>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('src/pages/blog'))
+  const posts = files
+    .filter(name => name.endsWith('.mdx'))
+    .map(filename => {
+      const slug = filename.replace('.mdx', '')
+      const markdown = fs.readFileSync(path.join('src/pages/blog', filename), 'utf-8')
+      const { data: frontmatter } = matter(markdown)
+      return { slug, frontmatter }
+    })
+
+  return { props: { posts } }
+}
